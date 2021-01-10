7d330abff090b29c4975408b429a7182062a5d21
leo870823 <b05602052@ntu.edu.tw>
2021-01-10 17:05:52 +0800

    txt issue

--- tb_files/generate.m
+++ tb_files/generate.m
@@ -1,4 +1,4 @@
-I_sharp = imresize(imread('.\DeconvolutionColorPrior\I_sharp.png'),[128 128]);
+I_sharp = imresize(imread('./DeconvolutionColorPrior/I_sharp.png'),[128 128]);
 R = I_sharp(:,:,1) ;
 G = I_sharp(:,:,2) ;
 B = I_sharp(:,:,3) ;
@@ -19,7 +19,7 @@ fprintf(fid, '%d\n', img1D);
 fclose(fid);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
-I_blurred = imresize(imread('.\DeconvolutionColorPrior\I_blurred.png'),[128 128]);
+I_blurred = imresize(imread('./DeconvolutionColorPrior/I_blurred.png'),[128 128]);
 R = I_blurred(:,:,1) ;
 G = I_blurred(:,:,2) ;
 B = I_blurred(:,:,3) ;
@@ -39,7 +39,7 @@ fid = fopen('I_blurred_B.txt', 'wt');
 fprintf(fid, '%d\n', img1D);
 fclose(fid);
 
-K_blur = imread('.\DeconvolutionColorPrior\kernels\fading.png');
+K_blur = imread('./DeconvolutionColorPrior/kernels/fading.png');
 K  = double(rgb2gray(K_blur)) ;
 K  = K ./ sum(K(:));
 otfk  = psf2otf(K, [128 128]); 
